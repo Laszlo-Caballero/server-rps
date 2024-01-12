b53136e883d8ff873805d9c9db55c3f62009bf1c
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-01-12 01:07:31 -0500

    fix info players

--- index.js
+++ index.js
@@ -1,62 +1,49 @@
-const express = require("express");
-const { createServer } = require("node:http");
-const { Server } = require("socket.io");
-const cors = require("cors");
-require("dotenv").config();
+import { Server } from "socket.io";
+import express from "express";
+import { createServer } from "node:http";
+import cors from "cors";
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET"],
-  },
+  cors: "*",
 });
-const port = process.env.ServerPort || 2000;
+
 app.use(cors());
 let activeRooms = [];
-
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("Create room", (roomName) => {
-    socket.join(roomName);
-    const index = activeRooms.findIndex(
-      (element) => element.roomName === roomName
-    );
+  console.log("a user conected");
+  socket.on("Create room", (room) => {
+    socket.join(room);
+    const index = activeRooms.findIndex((element) => element.room === room);
     if (index !== -1) {
       activeRooms[index].players++;
+      io.to(room).emit("players", activeRooms[index].players);
+      console.log(activeRooms[index].players);
     } else {
       activeRooms.push({
-        roomName,
-        players: io.sockets.adapter.rooms.get(roomName).size,
+        room,
+        players: 1,
       });
     }
     io.emit("New room", activeRooms);
   });
-  socket.on("Emit option", (roomName, option) => {
-    socket.to(roomName).emit("Option Game", option);
-  });
-  socket.on("Play again", (roomName, number) => {
-    socket.to(roomName).emit("Response Play", number);
-  });
+
   socket.on("leave room", (room) => {
     socket.leave(room);
     console.log("leave");
-    const index = activeRooms.findIndex((element) => element.roomName === room);
+    const index = activeRooms.findIndex((element) => element.room === room);
     let players;
     if (index != -1 && activeRooms[index].players > 0) {
       players = activeRooms[index].players;
       activeRooms[index].players--;
     }
-    io.to(room).emit("Info players", players);
+    io.to(room).emit("players", players);
     io.emit("New room", activeRooms);
   });
-  socket.on("Info", (room) => {
-    const index = activeRooms.findIndex((element) => element.roomName === room);
-    let players;
-    if (index != -1 && activeRooms[index].players > 0) {
-      players = activeRooms[index];
-    }
-    io.to(room).emit("Info players", players);
+
+  socket.on("Option", (room, option) => {
+    console.log(option, socket.id);
+    socket.broadcast.to(room).emit("Turn Opponent", option);
   });
 });
 
@@ -64,8 +51,16 @@ app.get("/rooms", (req, res) => {
   res.send(activeRooms);
 });
 
-server.listen(port, () => {
-  console.log(`server running at http://localhost:${port}`);
+app.get("/players/:room", (req, res) => {
+  const { room } = req.params;
+  const index = activeRooms.findIndex((element) => element.room === room);
+  res.send(
+    index != -1 ? { players: activeRooms[index].players } : { players: 1 }
+  );
+});
+
+server.listen(4000, () => {
+  console.log("Server on Port 4000");
 });
 
 setInterval(() => {
